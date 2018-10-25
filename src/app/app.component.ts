a16070b2b1980dbb2ca405ef61a8ff9ace90da94
Moody <sajjad.hassanpour@protonmail.com>
2018-10-25 15:01:25 +0200

    transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IOutput, IProps } from './interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'circular-slider';
+  public startDate: Date;
+  public endDate: Date;
+  public start = 0;
+  public length = 0;
+  public sliderProps = SLIDER_PROPS;
+
+  constructor() {
+    this.startDate = new Date();
+    this.endDate = new Date();
+  }
+
+  public handleSliderChange(changes: IOutput) {
+    console.log('changes in app ', changes);
+  }
+
+  public createRandomValues() {
+    console.log('changes in app randomo');
+    this.length += 0.33;
+  }
+
+  // todo: functions are for using it (demo)
+  private calculateMinutesFromAngle(angle) {
+    return Math.round(angle / ((2 * Math.PI) / (12 * 12))) * 5;
+  }
+
+  private calculateTimeFromAngle(angle) {
+    const minutes = this.calculateMinutesFromAngle(angle);
+    const h = Math.floor(minutes / 60);
+    const m = minutes - h * 60;
+
+    return { h, m };
+  }
+
+  private roundAngleToFives(angle) {
+    const fiveMinuteAngle = (2 * Math.PI) / 144;
+    return Math.round(angle / fiveMinuteAngle) * fiveMinuteAngle;
+  }
+
+  private padMinutes(min) {
+    if (`${min}`.length < 2) {
+      return `0${min}`;
+    }
+
+    return min;
+  }
 }
+
+
+
+
+
+const SLIDER_PROPS: IProps = {
+  gradientColorFrom: '#CCCC00',
+  gradientColorTo: '#CCCC00',
+  segments: 48,
+  bgCircleColor: 'yellow'
+};
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgXCSClockFaceComponent } from './ngx-cs-clock-face/ngx-cs-clock-face.component';
+import { NgxCircularSliderComponent } from './ngx-cs-slider/ngx-cs-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgXCSClockFaceComponent,
+    NgxCircularSliderComponent
   ],
   imports: [
     BrowserModule
